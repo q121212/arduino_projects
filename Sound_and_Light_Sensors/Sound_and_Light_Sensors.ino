65a5cac3539d4eff33ade8ac378a0f0711d4703d
Max R <q121212@gmail.com>
2017-08-06 14:32:47 +0300

    +Speaker threshold

--- Sound_and_Light_Sensors/Sound_and_Light_Sensors.ino
+++ Sound_and_Light_Sensors/Sound_and_Light_Sensors.ino
@@ -1,12 +1,15 @@
 #define Mic 0
 #define LightSensor 5
 #define LED 13
+#define Speaker 11
+
 char state=0;
 int valMic, valLight;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
+  pinMode(Speaker, OUTPUT);
 
   Serial.begin(9600);
   while (!Serial) 
@@ -24,8 +27,13 @@ void loop() {
     Serial.println(valMic);
   }
   // threshold of darkness: the higher the value - the darker
-  if (valLight>364){
+  if (valLight>370){
     Serial.print("Light value is: ");
     Serial.println(valLight);
+    if (valLight>500 && valLight<510){
+      digitalWrite(Speaker, HIGH);
+      delay(5);
+      digitalWrite(Speaker, LOW);
+    }
   }
 }
